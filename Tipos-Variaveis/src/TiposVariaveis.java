04ec4cbcd8229fe4b817ddb3c13e58c08b89df7a
rodrigo <rodrigosf494@gmail.com>
2022-10-10 16:41:48 -0300

    Exemplos de variavel

--- Tipos-Variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+		
+        //exemplos de varivel como novos valores
+        int numero = 5;
+        numero =10;
+        System.out.print("Novo valor da Variavel é = " + numero +"\n");
+        
+        //exmeplos de varivel que nunca vai mudar acompanhada do final
+        final double VALOR_DE_PI = 3.14;
+        System.out.print("Valor de PI é: " + VALOR_DE_PI);
+
+        
     }
+    
 }
