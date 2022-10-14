65bdc8c46779788979b17740833220283c666555
rodrigo <rodrigosf494@gmail.com>
2022-10-14 17:34:49 -0300

    aula metodos criando SmartTv

---
+++ Cuso-Java/src/Metodos/SmartTv.java
@@ -0,0 +1,41 @@
+public class SmartTv {
+
+        //atrbuindo valor as varias para mudar canal,volume e deligar e ligar tv.
+        boolean ligada = false;
+        int canal = 1;
+        int volume = 25;
+
+        //comando para mudar para novo canal.
+        public void mudarCanal(int novoCanal){
+            canal = novoCanal;
+        }
+        //comando para acrencentar mas um canal
+        public void aumentarCanal(){
+            canal++;
+        } 
+        //comando para diminuir mas um canal
+        public void diminuirCanal(){
+            canal--;
+        }
+        //comando para acrencentar mas um volume e imprimir na tela
+        public void aumentarVolume(){
+            volume++;
+            System.out.println("Aumentando o volume para :  " + volume);
+        }
+        //comando para diminuir mas um volume e imprimir na tela
+        public void diminuirVolume(){
+            volume--;
+            System.out.println("Diminuindo o volume para :  " + volume);
+        }
+        //comando para mudar status tv.
+        public void ligar(){
+            ligada = true;
+        }
+        //comando para mudar status tv.
+        public void desligar(){
+            ligada = false;
+        }
+    }
+    
+        
+   
---
+++ Cuso-Java/src/Metodos/Usuario.java
@@ -0,0 +1,24 @@
+public class Usuario {
+    public static void main (String[] args){
+        SmartTv smartTv = new SmartTv();
+        
+        //chamada para conferir status inical do pragrama tv.
+        System.out.println("Volume inicial:  " + smartTv.volume);
+        System.out.println("Canal inicial:  " + smartTv.canal);
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+            
+        //comando dado para conferir novas funcionalidade para tv
+        smartTv.diminuirVolume();       
+        smartTv.aumentarVolume();
+        smartTv.mudarCanal(20);
+        System.out.println("Canal atual:  " + smartTv.canal);
+        System.out.println("Volume atual:  " + smartTv.volume);
+             
+        //teste para ver se os comando de ligar e deligar mudirama os status.
+        smartTv.ligar();
+        System.out.println("Novo status -> TV Ligada ? " + smartTv.ligada);
+        smartTv.desligar();
+        System.out.println("Novo status -> TV Ligada ? " + smartTv.ligada);
+    }
+    
+}
