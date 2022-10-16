b049019fe13bb605d4aba6295c7b55d72663e2df
rodrigo <rodrigosf494@gmail.com>
2022-10-16 13:52:19 -0300

    mas um desafio feito

---
+++ Cuso-Java/src/Desafios/BuscaSequencial.java
@@ -0,0 +1,41 @@
+package Desafios;
+
+    // IMPORTANTE: Para ler os dados de entrada do usuário, utilize a classe Scanner, 
+    // instanciando seu leitor da seguinte forma: "Scanner leitor = new Scanner(System.in);". 
+    // Por outro lado, para imprimir suas saídas, utilize System.out.print ou System.out.println. 
+    
+    import java.util.*;
+    
+    public class BuscaSequencial {
+    
+        public static void main(String[] args) {
+    
+            Scanner leitor = new Scanner(System.in);
+    
+            int[] elementos = {64, 137, -16, 43, 67, 81, -90, 212, 10, 75};
+            //valor que irar ser buscado no sistema
+            System.out.println("Digite o valor a ser buscado: ");
+            int busca = leitor.nextInt();
+                      
+            int i;
+            
+            for(i = 0; i < elementos.length; i++){
+              if(elementos[i] == busca){
+                System.out.println("Achei " + busca + " na posicao " + i);
+                break;
+              }
+              
+            } 
+              if (i == elementos.length){
+                  System.out.println("Numero " + busca + " nao encontrado!");
+              }
+            
+            
+            
+    
+            //TODO: Retorne o valor do elemento no Array junto de sua respectiva posição.
+    
+        }
+    }
+    
+
